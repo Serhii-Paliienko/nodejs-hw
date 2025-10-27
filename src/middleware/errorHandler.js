654de7dad7d314a57aa198f89b678cd0d5d27747
Serhii_Paliienko <sergiopalienko@gmail.com>
2025-10-27 11:07:49 +0100

    update errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,9 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, _next) => {
   req.log?.error({ err }, 'Unhandled error');
-  const status = err.status || err.statusCode || 500;
-  res.status(status).json({ message: err.message });
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  return res.status(500).json({ message: err.message });
 };
